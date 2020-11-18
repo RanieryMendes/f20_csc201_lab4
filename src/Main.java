e681a6c77bfd451265fd4e6995604ad288b05f75
RanieryMendes <mendrc18@wfu.edu>
2020-11-18 00:19:45 -0500

    Commit #2 Optimal Method works. Need to clean code and make comments

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 
 
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
 
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
 
         /* an integer representing the capacity of the knapsack
@@ -16,42 +20,78 @@ the number of things available to pack
 a list of the weights of each of these things (integers)
 a list of the values of each of these things (integers)*/
 
-        int capacity = 200;
+        File txt = new File("/Users/ranierymendes/Documents/lab4/src/knapsack.txt");
 
-        int number_things= 50;
+        Scanner reader = new Scanner(txt);
 
-        int [] weight_things= new int[number_things];
+        int a;
+        int b;
 
-        int [] value_things = new int[number_things];
+        int[ ]w ;
 
-        ArrayList<Things> list_of_things = new ArrayList<>();
+        int [] v;
 
-        for (int i = 0; i < number_things ; i++) {
+        while (reader.hasNextLine()){
 
-            weight_things[i] = (int)(Math.random() * number_things-1);
-            value_things[i] = (int)(Math.random() * number_things-1);
+            a = Integer.parseInt(reader.next());
 
-            Things obj = new Things();
+            b=Integer.parseInt(reader.next());
 
-            obj.setWeight(weight_things[i]);
-            obj.setValue(value_things[i]);
+            w = new int[b];
 
-            list_of_things.add(obj);
+            v = new int[b];
 
-            System.out.println(i +": Weight:  " + weight_things[i] + " Value: " + value_things[i]);
+            for(int i=0; i<b; i++){
+                w[i]=Integer.parseInt(reader.next());
+            }
+
+            for(int i=0; i<b; i++){
+                v[i] = Integer.parseInt(reader.next());
+            }
+
+            for (int i = 0; i < b; i++) {
+                System.out.println("i: "+ i +" Weight: " + w[i] +" Value: " + v[i] );
+
+
+            }
+
+            System.out.println("Total capacity: " + a);
+            ArrayList<Things> list_of_things = new ArrayList<>();
+
+            for (int i = 0; i < b ; i++) {
+
+
+                Things obj = new Things();
+
+                obj.setWeight(w[i]);
+                obj.setValue(v[i]);
+
+                list_of_things.add(obj);
+
+                System.out.println(i +": Weight:  " + w[i] + " Value: " + v[i]);
+
+            }
+
+            knapsack knapsack = new knapsack(a, list_of_things);
+
+            knapsack knapsack1 = new knapsack(a, list_of_things);
+
+
+
+
+            ArrayList<Integer> taken  =new ArrayList<Integer>();
+            System.out.println(list_of_things.size());
+
+            knapsack.greedyFill();
+            knapsack1.optimalFill();
+
+
+
 
         }
 
-        knapsack knapsack = new knapsack(capacity, list_of_things);
 
 
-
-
-        System.out.println(list_of_things.size());
-
-        knapsack.greedyFill();
-
-        knapsack.optimalFill();
 
 
 
--- src/knapsack.java
+++ src/knapsack.java
@@ -1,49 +1,52 @@
 
 import java.util.ArrayList;
 
+
 import java.util.Arrays;
 
 public class knapsack {
 
     ArrayList<Things> bag;
 
-    int sac_capacity  = 0;
+    int sac_capacity = 0;
 
     int current_capacity = 0;
 
     int value;
 
     int length_list;
+    Things[] forOptimal;
 
-    Things [] listOfThings;
+    Things[] listOfThings;
 
 
-    knapsack (int total_capacity, ArrayList<Things> list){
-       bag  = new ArrayList<>();
-       length_list = list.size();
-       sac_capacity = total_capacity;
-       value = 0;
-       listOfThings = list.toArray(new Things[list.size()]);
+    knapsack(int total_capacity, ArrayList<Things> list) {
+        bag = new ArrayList<>();
+        length_list = list.size();
+        sac_capacity = total_capacity;
+        value = 0;
+        forOptimal = list.toArray(new Things[list.size()]);
+        listOfThings = list.toArray(new Things[list.size()]);
 
 
     }
 
 
-    public void greedyFill(){
+    public void greedyFill() {
 
         System.out.println("Performing the greedyFill Method");
 
         Arrays.sort(listOfThings);
 
-        for (int i=0; i< listOfThings.length; i++){
+        for (int i = 0; i < listOfThings.length; i++) {
             System.out.println("i:" + i + " value: " + listOfThings[i].getValue() + " weight: " + listOfThings[i].getWeight());
         }
 
-        int i=0;
-        for(int j=0; j<listOfThings.length; j++){
+        int i = 0;
+        for (int j = 0; j < listOfThings.length; j++) {
 
 
-            if((current_capacity + listOfThings[i].getWeight()) < sac_capacity) {
+            if ((current_capacity + listOfThings[i].getWeight()) < sac_capacity) {
 
                 bag.add(listOfThings[i]);
 
@@ -56,57 +59,127 @@ public class knapsack {
 
         System.out.println(" ");
 
-        System.out.println("That is my bag. Its total weight is:  " + current_capacity );
-        for(int t=0; t<bag.size(); t++){
+        System.out.println("That is my bag. Its total weight is:  " + current_capacity);
+        for (int t = 0; t < bag.size(); t++) {
             System.out.println("t:" + t + " value: " + bag.get(t).getValue() + " weight:" + bag.get(t).getWeight());
         }
 
 
     }
 
-    public void optimalFill(){
+    public void optimalFill() {
 
         System.out.println("Performing optimal Fill: ");
 
-        int [] array_of_weights = new int[length_list];
+        int[] array_of_weights = new int[length_list];
 
-        for(int i=0; i<length_list; i++){
+        for (int i = 0; i < length_list; i++) {
             array_of_weights[i] = listOfThings[i].getWeight();
         }
 
-        int [] array_of_values = new int[length_list];
+        int[] array_of_values = new int[length_list];
 
 
-        for(int i=0; i<length_list; i++){
-           array_of_values[i] = listOfThings[i].getValue();
+        for (int i = 0; i < length_list; i++) {
+            array_of_values[i] = listOfThings[i].getValue();
         }
 
 
-        System.out.println(recursivePart(array_of_weights, array_of_values, length_list, sac_capacity));
+       // System.out.println(knapsack(array_of_weights, array_of_values, sac_capacity, 0));
 
+        for (int i = 0; i < bag.size(); i++) {
+
+            System.out.println("Bag poderosa: " + bag.size() + " items");
+
+            System.out.println("i: " + i + " Value: " + bag.get(i).getValue() + "  Weight: " + bag.get(i).getWeight());
+
+        }
+        System.out.println();
+
+        ArrayList<Things> taken = new ArrayList<Things>();
+        System.out.println( "Total value in my knapsack: " +KnapSack(sac_capacity, forOptimal, forOptimal.length, taken));
+
+        System.out.println("There are "+ taken.size() +  " items inside the knapsack: ");
+
+        for (int i = 0; i< taken.size(); i++){
+            System.out.println("This is my weight: " +taken.get(i).getWeight() + " This is my value:  " +taken.get(i).getValue());
+        }
 
 
     }
 
+    //change code
 
-    public int recursivePart(int [] weight, int[] value, int number_of_things, int sac_capacity){
+    public int knapsack(int[] weight, int[] val, int w, int itemNum) {
+        if (w == 0 || itemNum == weight.length) {
 
-       if(number_of_things <=0){
-           return 0;
-       }
-
-       if(weight[number_of_things-1] > sac_capacity){
-            return  recursivePart(weight, value, number_of_things-1, sac_capacity);
-       }
-
-       else{
-           return Math.max(recursivePart(weight, value,
-                   number_of_things-1, sac_capacity),
-                   (value[number_of_things-1]+ recursivePart(weight, value, number_of_things-1, sac_capacity-weight[number_of_things-1])));
-       }
+            if (w == 0) {
+                System.out.println(weight[itemNum] + "  val: " + val[itemNum]);
+            }
 
 
+            return 0;
+        }
 
-    };
+        if (weight[itemNum] > w) {
 
+
+            return knapsack(weight, val, w, itemNum + 1);
+        } else {
+
+            int rMax = val[itemNum] + knapsack(weight, val, w - weight[itemNum], itemNum + 1);
+            int lMax = knapsack(weight, val, w, itemNum + 1);
+
+            if (rMax == Math.max(rMax, lMax)) {
+
+                System.out.println("ADICIONAR: RIGHT " + weight[itemNum]);
+            }
+
+            if (lMax == Math.max(rMax, lMax)) {
+                System.out.println("ADICIONAR: LEFT " + lMax);
+            }
+            //System.out.println( "O maior:  "+ Math.max(rMax, lMax));
+
+
+            return Math.max(rMax, lMax);
+        }
+    }
+
+
+    public int KnapSack(int capacity, Things[] items, int numItems, ArrayList<Things> taken) {
+        System.out.println("Current Capacity" + capacity);
+
+
+        if (numItems == 0 || capacity == 0)
+            return 0;
+        if (items[numItems - 1].getWeight() > capacity)
+            return KnapSack(capacity, items, numItems - 1, taken);
+        else {
+            final int preTookSize = taken.size();
+            final int took = items[numItems - 1].getValue() + KnapSack(capacity - items[numItems - 1].getWeight(), items, numItems - 1, taken);
+
+            final int preLeftSize = taken.size();
+            final int left = KnapSack(capacity, items, numItems - 1, taken);
+
+            if (took > left) {
+                if (taken.size() > preLeftSize)
+                    taken.subList(preLeftSize, taken.size()).clear();
+                    //taken.removeRange(preLeftSize, taken.size());
+
+                //taken.add(Integer.valueOf(numItems - 1));
+                taken.add(items[numItems-1]);
+
+                taken.indexOf(items);
+                return took;
+            } else {
+                if (preLeftSize > preTookSize)
+                    taken.subList(preTookSize, preLeftSize).clear();
+                   // taken.removeRange(preTookSize, preLeftSize);
+
+                return left;
+            }
+
+        }
+
+    }
 }
